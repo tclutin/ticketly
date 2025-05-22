02bd9b9b22a57aca59bd914b38fbd06285a617a3
Lutin <tclutin01@gmail.com>
2025-05-22 05:48:01 +0500

    feat: users's table

--- ticketly-api/migrations/20250521140401_init.sql
+++ ticketly-api/migrations/20250521140401_init.sql
@@ -1,9 +1,14 @@
 -- +goose Up
 -- +goose StatementBegin
-SELECT 'up SQL query';
+CREATE TABLE IF NOT EXISTS public.users (
+    user_id BIGSERIAL PRIMARY KEY,
+    external_id VARCHAR(200) NOT NULL,
+    source VARCHAR(100) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-SELECT 'down SQL query';
+DROP TABLE IF EXISTS public.users;
 -- +goose StatementEnd
