f26f98196f6453edb32eada979f84113bb68b417
Lutin <tclutin01@gmail.com>
2025-05-22 21:53:34 +0500

    feat: add ticket model and base ticket repository

---
+++ ticketly-api/internal/models/ticket.go
@@ -0,0 +1,14 @@
+package models
+
+import "time"
+
+type Ticket struct {
+	TicketID   uint64
+	UserID     uint64
+	OperatorID *uint64
+	Status     string
+	Type       string
+	Sentiment  string
+	CreatedAt  time.Time
+	ClosedAt   time.Time
+}
--- ticketly-api/internal/repository/repository.go
+++ ticketly-api/internal/repository/repository.go
@@ -1,1 +1,11 @@
 package repository
+
+import (
+	"context"
+	"github.com/tclutin/ticketly/ticketly_api/internal/models"
+)
+
+type UserRepository interface {
+	Create(ctx context.Context, user models.User) error
+	GeByExternalId(ctx context.Context, externalId string) (models.User, error)
+}
