a849c2413667cc8eee641706d0ab839e6efd2e29
Lutin <tclutin01@gmail.com>
2025-05-23 04:54:57 +0500

    refactor: app.go

--- ticketly-api/internal/app/app.go
+++ ticketly-api/internal/app/app.go
@@ -2,12 +2,23 @@ package app
 
 import (
 	"context"
+	"errors"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/tclutin/ticketly/ticketly_api/internal/config"
+	http2 "github.com/tclutin/ticketly/ticketly_api/internal/delivery/http"
 	rabbitmq2 "github.com/tclutin/ticketly/ticketly_api/internal/delivery/rabbitmq"
+	messageRepository "github.com/tclutin/ticketly/ticketly_api/internal/repository/message"
+	ticketRepository "github.com/tclutin/ticketly/ticketly_api/internal/repository/ticket"
+	userRepository "github.com/tclutin/ticketly/ticketly_api/internal/repository/user"
+	ticketService "github.com/tclutin/ticketly/ticketly_api/internal/service/ticket"
+	userService "github.com/tclutin/ticketly/ticketly_api/internal/service/user"
 	"github.com/tclutin/ticketly/ticketly_api/pkg/client/postgres"
 	"github.com/tclutin/ticketly/ticketly_api/pkg/client/rabbitmq"
+	"net"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 type App struct {
@@ -28,19 +39,55 @@ func New() *App {
 		cfg.RabbitMQ.ToOperatorQueue,
 	)
 
-	_ = rabbitmq2.NewPublisher(rabbitmqClient.Ch(), cfg.RabbitMQ.Exchange)
+	publisher := rabbitmq2.NewPublisher(rabbitmqClient.Ch(), cfg.RabbitMQ.Exchange)
+
+	//users stuff
+	userRepo := userRepository.NewRepository(postgresClient)
+
+	userSrv := userService.NewService(userRepo)
+
+	//messages stuff
+	messageRepo := messageRepository.NewRepository(postgresClient)
+
+	//tickets stuff
+	ticketRepo := ticketRepository.NewRepository(postgresClient)
+
+	ticketSrv := ticketService.NewService(ticketRepo, userRepo, messageRepo, publisher)
+
+	router := http2.InitRouter(userSrv, ticketSrv)
 
 	return &App{
 		pgClient:       postgresClient,
 		rabbitmqClient: rabbitmqClient,
+		httpServer: &http.Server{
+			Addr:    net.JoinHostPort(cfg.HTTP.Host, cfg.HTTP.Port),
+			Handler: router,
+		},
 	}
 }
 
 func (a *App) Run() {
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
 
+	go func() {
+		err := a.httpServer.ListenAndServe()
+		if err != nil && errors.Is(err, http.ErrServerClosed) {
+			//app.logger.Error("Server stopped with error", "error", err)
+			os.Exit(1)
+		}
+	}()
+
+	<-quit
+	a.Stop()
 }
 
 func (a *App) Stop() {
 	a.rabbitmqClient.Close()
 	a.pgClient.Close()
+
+	if err := a.httpServer.Shutdown(context.Background()); err != nil {
+		//a.logger.Error("Error during server shutdown", "error", err)
+		os.Exit(1)
+	}
 }
