f2894a795901fd18db8ccec1c0a1569d67adb686
Lutin <tclutin01@gmail.com>
2025-05-22 21:37:52 +0500

    feat: add tickets and messages tables

---
+++ ticketly-api/migrations/20250522161044_add_tickets.sql
@@ -0,0 +1,28 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS public.tickets (
+    ticket_id BIGSERIAL PRIMARY KEY,
+    user_id BIGINT NOT NULL,
+    operator_id BIGINT,
+    status VARCHAR(100) NOT NULL CHECK (status IN ('open', 'in_progress', 'closed')) DEFAULT 'open',
+    type VARCHAR(100) NOT NULL CHECK (type IN ('only-message', 'realtime-chat')),
+    sentiment VARCHAR(100) CHECK (sentiment IN ('positive', 'neutral', 'negative')),
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    closed_at TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS public.messages (
+    message_id BIGSERIAL PRIMARY KEY,
+    ticket_id BIGINT NOT NULL,
+    sender_type VARCHAR(20) CHECK (sender_type IN ('user', 'operator', 'system')),
+    content TEXT NOT NULL,
+    sentiment VARCHAR(100) CHECK (sentiment IN ('positive', 'neutral', 'negative')),
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS public.messages;
+DROP TABLE IF EXISTS public.tickets;
+-- +goose StatementEnd
