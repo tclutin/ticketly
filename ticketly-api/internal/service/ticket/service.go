d659586dbd53c3d11344a1ea0df0332577ec6218
Lutin <tclutin01@gmail.com>
2025-05-30 03:08:58 +0500

    feat: add check for existing active realtime ticket before creation

--- ticketly-api/internal/service/ticket/service.go
+++ ticketly-api/internal/service/ticket/service.go
@@ -50,12 +50,14 @@ func (s *Service) Create(ctx context.Context, dto CreateTicketDTO) (uint64, erro
 		return 0, err
 	}
 
-	exists, err := s.repo.HasActiveRealtimeTicket(ctx, dto.UserID)
-	if err != nil {
-		return 0, err
-	}
-	if exists {
-		return 0, coreerrors.ErrActiveTicketAlreadyExists
+	if dto.Type == "realtime-chat" {
+		exists, err := s.repo.HasActiveRealtimeTicket(ctx, dto.UserID)
+		if err != nil {
+			return 0, err
+		}
+		if exists {
+			return 0, coreerrors.ErrActiveTicketAlreadyExists
+		}
 	}
 
 	ticket := models.Ticket{
